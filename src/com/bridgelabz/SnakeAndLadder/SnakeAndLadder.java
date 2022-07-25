3ef3ba8ae86bea8db8a0a66db9d43ddd7f3a2002
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-25 06:38:14 +0530

    [Shreyash]: added welcome message

---
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.SnakeAndLadder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Snake And Ladder game");
+		
+	}
+
+}
