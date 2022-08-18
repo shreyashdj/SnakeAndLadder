00fa7e8755e4b2e7776bd7cfdac75e24633fca79
shreyashdj <shreyashjadhav1799@gmail.com>
2022-08-18 18:58:22 +0530

    [Shreyash]:Added program to used RANDOM to get dice output 1 to 6

--- src/com/bridgelabz/SnakeAndLadder/Play.java
+++ src/com/bridgelabz/SnakeAndLadder/Play.java
@@ -2,12 +2,14 @@ package com.bridgelabz.SnakeAndLadder;
 
 public class Play {
 
-	int playerPosition;
+	int playerPosition, dice;
 	
 	void game() {
 		
 		System.out.println(" Player Position : " + playerPosition);
 		
+		dice = (int) (Math.random() * 6 + 1);
+        System.out.println(" Dice : "+dice);
 	}
 	
-	}
+}
