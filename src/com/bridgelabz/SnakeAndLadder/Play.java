c5eecfb26acb61b32f06dee0b801ef609868e1e1
shreyashdj <shreyashjadhav1799@gmail.com>
2022-08-18 18:44:47 +0530

    [Shreyash]:Added program to start game with starting position zero

---
+++ src/com/bridgelabz/SnakeAndLadder/Play.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.SnakeAndLadder;
+
+public class Play {
+
+	int playerPosition;
+	
+	void game() {
+		
+		System.out.println(" Player Position : " + playerPosition);
+		
+	}
+	
+	}
--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -1,11 +1,14 @@
 package com.bridgelabz.SnakeAndLadder;
 
 public class SnakeAndLadder {
-
+	
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Snake And Ladder game");
+		System.out.println("Welcome to Snake And Ladder game \n");
+		
+		Play play = new Play();
+		
+		play.game();
 		
 	}
-
 }
