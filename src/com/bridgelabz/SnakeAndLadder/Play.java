1f363506396d9070b087994058e03bbddac65b1e
shreyashdj <shreyashjadhav1799@gmail.com>
2022-08-18 19:11:23 +0530

    [Shreyash]:Added program to check optons for next move using RANDOM & switch case

--- src/com/bridgelabz/SnakeAndLadder/Play.java
+++ src/com/bridgelabz/SnakeAndLadder/Play.java
@@ -2,14 +2,31 @@ package com.bridgelabz.SnakeAndLadder;
 
 public class Play {
 
-	int playerPosition, dice;
+	int playerPosition, dice, option;
 	
 	void game() {
 		
-		System.out.println(" Player Position : " + playerPosition);
+		System.out.println(" Player starting Position : " + playerPosition);
 		
 		dice = (int) (Math.random() * 6 + 1);
-        System.out.println(" Dice : "+dice);
+        System.out.println("\n Dice : "+dice);
+        
+        option = (int) (Math.random() * 3);
+
+        switch (option) {
+	            case (1) -> {
+	                System.out.println(" Case : Ladder");
+	                playerPosition += dice;
+	            }
+	            case (2) -> {
+	                System.out.println(" Case : Snake");
+	                playerPosition -= dice;
+	            }
+	            default -> System.out.println(" Case : No Play");
+	        }
+        
+        System.out.println("\n Player Position : " + playerPosition);
+        
 	}
 	
 }
